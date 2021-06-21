00c1799382397deb6147ef67ba0e9ad0a521fcb5
Karl Sander Erss <karl.erss@gmail.com>
2021-06-21 18:06:00 +0300

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div style="padding-left: 2rem;">
+    <p>
+      Please build a component for rating candidates. An example use case would be hiring
+      managers giving their opinion on candidate fit for a position. The component should emit
+      a numeric value in range [0; 100] or null, if no value is selected. The component should
+      have basic styles and be fully functional. Implement the component in components/Rating.vue.
+    </p>
+    <ul>
+      <li>Make sure the component can not emit invalid values.</li>
+      <li>Feel free to include 3rd party code, if necessary.</li>
+      <li>Try to keep the implementation time below 2h (excluding reading
+        <a href="https://v3.vuejs.org/guide/introduction.html" target="_blank">docs</a>).
+        Make necessary tradeoffs.</li>
+      <li>Let's discuss if you have any questions.</li>
+    </ul>
+    <div class="d-flex" style="background-color: #ededed; padding: 2rem; max-width: 60ch;">
+      <div>
+        Rating
+      </div>
+      <rating
+        v-model="rating"
+      ></rating>
+    </div>
+    <p v-if="rating === null">
+      No rating
+    </p>
+    <p v-else>
+      Current rating is {{ rating }}
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Rating from './components/Rating.vue'
 
 export default defineComponent({
   name: 'App',
+  data () {
+    return {
+      rating: 42
+    }
+  },
   components: {
-    HelloWorld
+    Rating
   }
 })
 </script>
 
 <style lang="scss">
+
+body {
+  line-height: 1.6;
+}
+
+p, ul {
+  max-width: 60ch;
+}
+
+.d-flex {
+  display: flex;
+  align-items: center;
+
+  > * {
+    margin-right: 1rem;
+  }
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Rating.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="range" min="0" max="100"
+           :value="modelValue"
+           @input="$emit('update:modelValue', parseInt($event.target.value))"
+    >
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: 'Rating',
+  emits: {
+    'update:modelValue' (val: number | null) {
+      if (val === null) {
+        return true
+      } else if (val && val >= 0 && val <= 100) {
+        return true
+      }
+      return false
+    }
+  },
+  props: {
+    modelValue: {
+      type: Number as PropType<number | null>
+    }
+  }
+})
+</script>
